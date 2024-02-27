6046467417a964bc59d5df9b68a2da7825f87d23
behzadarbab <behzadarbab@yahoo.com>
2024-02-27 11:55:41 +0100

    gather functions in training_func and plotting_func modules and fix some parts of the model
    - created training_func and plotting func and gathered files
    	Reason: it is easier to track chnages and reading the main file is easier to the eye
    - fixed small parts that differed from the tutorial. for example, zeroing the optimizer in the training loop and using rml.train.
    	I also changed the xscale of the loss per iteration plot
    - played with the config parameters to get a good final image.

---
+++ scripts/plotting_func.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_final_image(rml, img_cube):
+    fig, ax = plt.subplots(nrows=1)
+    im = ax.imshow(
+        np.squeeze(img_cube),
+        origin="lower",
+        interpolation="none",
+        extent=rml.icube.coords.img_ext,
+    )
+    ax.set_xlabel(r"$\Delta \alpha \cos \delta$ [${}^{\prime\prime}$]")
+    ax.set_ylabel(r"$\Delta \delta$ [${}^{\prime\prime}$]")
+    ax.set_title("Maximum likelihood image")
+    plt.colorbar(im, label=r"Jy/$\mathrm{arcsec}^2$")
+    plt.savefig('RML_loop_outputs/maximum_likelihood_image.pdf', format='pdf', bbox_inches='tight')
+    print(f'Maximum likelihood image plot saved to: RML_loop_outputs/maximum_likelihood_image.pdf\n')
+    plt.close()
+
+def plot_loss_per_iter(hyperparams_config, loss_tracker):
+    fig, ax = plt.subplots(nrows=1)
+    ax.plot(np.arange(hyperparams_config["epochs"]), loss_tracker, marker=".", color="k", linewidth=0.5)
+    ax.set_xlabel("Iteration")
+    ax.set_xscale("log")
+    ax.set_ylabel("Loss")
+    ax.set_title("Loss per iteration")
+    plt.savefig('RML_loop_outputs/loss_per_iteration.pdf', format='pdf', bbox_inches='tight')
+    print(f'Loss per iteration plot saved to: RML_loop_outputs/loss_per_iteration.pdf\n')
+    plt.close()
+
+def plot_dirty(imager, img, beam, chan):
+    kw = {"origin": "lower", "interpolation": "none", "extent": imager.coords.img_ext}
+    fig, ax = plt.subplots(ncols=2, figsize=(6, 3))
+    bmplot = ax[0].imshow(beam[chan], **kw)
+    #plt.colorbar(bmplot, ax=ax[0])
+    ax[0].set_title("Dirty beam")
+    imgplot = ax[1].imshow(img[chan], **kw)
+    #plt.colorbar(imgplot, ax=ax[1])
+    ax[1].set_title("Dirty image")
+    for a in ax:
+        a.set_xlabel(r"$\Delta \alpha \cos \delta$ [${}^{\prime\prime}$]")
+        a.set_ylabel(r"$\Delta \delta$ [${}^{\prime\prime}$]")
+    plt.tight_layout()
+    plt.savefig('RML_loop_outputs/dirty_beam_and_dirty_image.pdf', format='pdf', bbox_inches='tight')
+    print(f'Dirty beam and dirty image plot saved to: RML_loop_outputs/dirty_beam_and_dirty_image.pdf')
+    plt.close()
+    print(f'The dirty image contains {np.sum(img < 0)} negative pixels.\n')
+
+def plot_uv_distribution(uu, vv):
+    fig, ax = plt.subplots(nrows=1)
+    ax.scatter(uu, vv, s=1, rasterized=True, linewidths=0.0, c="k")
+    ax.set_xlabel(r"$u$ [k$\lambda$]")
+    ax.set_ylabel(r"$v$ [k$\lambda$]")
+    ax.set_title("uv distribution")
+    plt.savefig('RML_loop_outputs/uv_distribution.pdf', format='pdf', bbox_inches='tight')
+    print(f'(u,v) distribution plot saved to: RML_loop_outputs/uv_distribution.pdf\n')
+    plt.close()
---
+++ scripts/training_func.py
@@ -0,0 +1,89 @@
+import torch
+from mpol import precomposed, losses
+import numpy as np
+import matplotlib.pyplot as plt
+
+def seed_from_dirty_image(learning_rate_dim, n_iter_dim, coords, img, dset, plot_loss_per_iteration=True, plot_final_seed=True):
+    """
+    use the dirty image as the initial model image in BaseCube
+    create a loss function corresponding to the mean squared error (MSE) between the RML model image pixel fluxes 
+    and the dirty image pixel fluxes and then optimize this RML model. It calculates the loss based off of the 
+    image-plane distance between the dirty image and the state of the ImageCube in order to make the state of the 
+    ImageCube closer to the dirty image.
+    """
+    print(f"Starting the optimisation loop with {n_iter_dim} iterations to optimise the initial model image (BaseCube) \
+          based on the dirty image...")
+    dirty_image = torch.tensor(img.copy())  # converts the dirty image into a pytorch tensor
+    rml_dim = precomposed.SimpleNet(coords=coords, nchan=dset.nchan) # initialise SimpleNet
+    optimizer_dim = torch.optim.SGD(rml_dim.parameters(), lr=learning_rate_dim) # instantiate the SGD optimizer
+
+    loss_tracker_dim = []
+    for i_dim in range(n_iter_dim):
+        optimizer_dim.zero_grad() # zero out any gradients attached to the tensor components so that they aren’t counted twice
+        rml_dim() # calculate the model visibilities from the current model image
+        sky_cube_dim = rml_dim.icube.sky_cube # get the model image from the BaseCube object
+        lossfunc_dim = torch.nn.MSELoss(reduction="sum")  # the MSELoss calculates mean squared error (squared L2 norm)
+        loss_dim = (lossfunc_dim(sky_cube_dim, dirty_image)) ** 0.5 # square root of the MSE is our loss value
+        loss_tracker_dim.append(loss_dim.item()) # append the loss value to the loss tracker list
+        loss_dim.backward() # calculate the gradients of the loss with respect to the model parameters
+        optimizer_dim.step() # subtract the gradient image to the base image in order to advance base parameters in the direction of the minimum loss value
+
+    # plot the loss per iteration
+    if plot_loss_per_iteration:
+        fig, ax = plt.subplots(nrows=1)
+        ax.plot(loss_tracker_dim)
+        ax.set_xlabel("iteration")
+        ax.set_ylabel("loss")
+        ax.set_title("loss per iteration - L2 norm (MSE) between dirty image and BaseCube model image")
+        plt.savefig('RML_loop_outputs/loss_per_iteration_dim.pdf', format='pdf', bbox_inches='tight')
+        print(f'Loss per iteration (for optimising the BaseCube image to the dirty image) plot saved to: RML_loop_outputs/loss_per_iteration_dim.pdf')
+        plt.close()
+
+    # plot the final model image (BaseCube) after the last iteration
+    if plot_final_seed:
+        img_dim = np.squeeze(rml_dim.icube.sky_cube.detach().numpy())
+        fig, ax = plt.subplots(nrows=1)
+        im_dim = ax.imshow(img_dim, origin="lower", interpolation="none", extent=rml_dim.icube.coords.img_ext)
+        plt.colorbar(im_dim)
+        plt.savefig('RML_loop_outputs/optimised_input_model_image_based_on_dirty_image.pdf', format='pdf', bbox_inches='tight')
+        print(f'Loss per iteration (for optimising the initial BaseCube to the dirty image) plot saved to: RML_loop_outputs/optimised_input_model_image_based_on_dirty_image.pdf')
+        plt.close()
+        print(f'The optimised initial image based on the dirty image contains {np.sum(img_dim < 0)} negative pixels.')
+
+        # save the optimised initial model image (BaseCube) to a .pt file
+    torch.save(rml_dim.state_dict(), "RML_loop_outputs/dirty_image_model.pt")
+    print('Optimised initial model image (BaseCube) based on the dirty image saved to: RML_loop_outputs/dirty_image_model.pt\n')
+    return rml_dim
+
+
+def train(hyperparams_config, dset, rml, optimizer):
+    # initiate a list to store the loss values at each iteration
+    loss_tracker = []
+    # NOTE: there was no rml.train here, but in the tutorial, it's written model.train()
+    rml.train() # set the model to training mode
+    for i in range(hyperparams_config["epochs"]):
+        # NOTE: it was rml.zero_grad() before, but in the tutorial it is written as optimizer.zero_grad()
+        optimizer.zero_grad()
+        # rml.zero_grad()
+
+        # STEP 1: calculate the model visibilities from the current model image
+        vis = rml() # calculate model visibilities
+        sky_cube = rml.icube.sky_cube # get the current model 'sky' image
+
+        # STEP 2: calculate the loss between the model visibilities and the data visibilities
+        # loss = losses.nll_gridded(vis, dset) # loss function without regularizers, using only the NLL
+        loss = (
+            losses.nll_gridded(vis, dset)
+            + hyperparams_config["lambda_sparsity"] * losses.sparsity(sky_cube)
+            + hyperparams_config["lambda_TV"] * losses.TV_image(sky_cube)
+            + hyperparams_config["entropy"] * losses.entropy(sky_cube, hyperparams_config["prior_intensity"])
+            + hyperparams_config["TSV"] * losses.TSV(sky_cube)
+        ) # loss function with regularizers #TODO: Implement regularizers properly and check how to get best hyperparameter values
+        loss_tracker.append(loss.item()) # append the loss value to the loss tracker list
+
+        # STEP 3: calculate the gradients of the loss with respect to the model parameters
+        loss.backward()
+
+        # STEP 4: subtract the gradient image to the base image in order to advance base parameters in the direction of the minimum loss value
+        optimizer.step()
+    return loss_tracker
